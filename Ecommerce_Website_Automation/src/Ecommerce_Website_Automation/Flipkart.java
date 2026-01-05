9398aa1c4b3560f425b90bb36e1c71af34ceb3b7
user <user@DESKTOP-S7USJ4A>
2026-01-05 09:42:39 +0530

    Added Class

---
+++ Ecommerce_Website_Automation/src/Ecommerce_Website_Automation/Flipkart.java
@@ -0,0 +1,10 @@
+package Ecommerce_Website_Automation;
+
+public class Flipkart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
+}
